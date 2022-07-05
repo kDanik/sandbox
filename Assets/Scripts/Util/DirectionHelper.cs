68bd421e1f48693efa78cb4946d4083a2be1e449
kDanik <daniilkurachkin@gmail.com>
2022-07-05 19:21:08 +0200

    Some more documentation added

---
+++ Assets/Scripts/Util/DirectionHelper.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// Enum for direction used in ElementReactions reactions for which direction is relevant
+/// </summary>
+public enum DirectionHelper
+{
+    Right,
+    Left,
+    Up,
+    Down
+}
--- Assets/Scripts/Util/WeightedRandom.cs
+++ Assets/Scripts/Util/WeightedRandom.cs
@@ -1,28 +1,33 @@
 ﻿using UnityEngine;
 
-public class WeightedRandom
+public static class WeightedRandom
 {
 
-    // takes as input dictionary of: key - position (x, y) and unit weight
-    // returns weighted random position
+    /// <summary>
+    /// Returns random position around xCenter, yCenter generated with weighted random from positionWeights.
+    /// </summary>
     public static (int x, int y) GetRandomPosition(uint[,] positionWeights, int xCenter, int yCenter)
     {
 
         uint weightSum = 0;
 
+        // calculate weightSum from positionWeights
         for (int y = 0; y <= 2; y++)
         {
             for (int x = 0; x <= 2; x++)
             {
-                weightSum += positionWeights[x,y];
+                weightSum += positionWeights[x, y];
             }
         }
-           
+
 
         uint randomValue = (uint)Random.Range(1, weightSum);
 
 
-        for (int y = 0; y <= 2; y++) {
+        // Iterate through positionWeights and extract weight from randomValue until weight is bigger than randomValue.
+        // Then return position connected with this weight
+        for (int y = 0; y <= 2; y++)
+        {
 
             for (int x = 0; x <= 2; x++)
             {
@@ -35,9 +40,11 @@ public class WeightedRandom
                 randomValue -= weight;
             }
         }
+
         return (xCenter, yCenter);
     }
 
+    // Calculates position on elementGrid from position of element in the center (xCenter,yCenter) and index of element from positionWeights(x,y)
     private static (int x, int y) GetPositionFromIndex(int x, int y, int xCenter, int yCenter)
     {
         return (xCenter - 1 + x, yCenter + 1 - y);
